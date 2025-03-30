9d117b8c63e919743e919f43af21acf17ffcc68b
Rumus Bajracharya <rumus.3353@gmail.com>
2025-03-30 10:55:00 -0400

    Initial Commit

---
+++ src/Pyramid.java
@@ -0,0 +1,10 @@
+package src;
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
