e9e50eeeafd58fb05b1075b755c499f2831bd486
Rumus Bajracharya <rumus.3353@gmail.com>
2025-04-10 12:50:29 -0400

    Generics: Generic functions, multi generic params, bounded by type generic functions

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -1,5 +1,7 @@
 package src.generics;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
         GenericPrinter<String> stringPrinter = new GenericPrinter<>("This is a string");
@@ -14,5 +16,44 @@ public class Main {
 
         BoundedGenericPrinter<Integer> bIntergerPrinter = new BoundedGenericPrinter<>(1999);
         bIntergerPrinter.print();
+
+        testingGenericSwap();
+        System.out.println(boundedTypeParamterGenericMethod(1.1f, 2.3f));
+        System.out.println(boundedTypeParamterGenericMethod(1, 2));
+        System.out.println(boundedTypeParamterGenericMethod(1000L, 2000000L));
+
+        multiParameterGenericMethod(1, "String", 5.55D);
+        multiParameterGenericMethod(10000L, 'r', 3.14f);
+    }
+
+    public static void testingGenericSwap(){
+        Integer[] intArr = {1,2,3,4,5,6};
+        System.out.println("Array values before swapping: " + Arrays.toString(intArr));
+        genericSwapMethod(intArr, 1, 2);
+        System.out.println("Array after before swapping: " + Arrays.toString(intArr));
+
+        String[] strArr = {"Apple","Banana", "Cherry"};
+        System.out.println("Array values before swapping: " + Arrays.toString(strArr));
+        genericSwapMethod(strArr, 1, 2);
+        System.out.println("Array after before swapping: " + Arrays.toString(strArr));
+
+
+    }
+
+
+    public static <T> void genericSwapMethod(T[] arr, int index1, int index2){
+        T temp = arr[index2];
+        arr[index2] = arr[index1];
+        arr[index1] = temp;
+    }
+
+    public static <T,U,V> void multiParameterGenericMethod(T varT, U varU, V varV){
+        System.out.println("Variable T: " +  varT + " Is of type: " + varT.getClass().getSimpleName());
+        System.out.println("Variable U: " + varU + " Is of type: " + varU.getClass().getSimpleName());
+        System.out.println("Variable V: " + varV + " Is of type: " + varV.getClass().getSimpleName());
+    }
+
+    public static <T extends Number> double boundedTypeParamterGenericMethod(T num1, T num2){
+        return num1.doubleValue() + num2.doubleValue();
     }
 }
